595c3d53c44d3152004cdcf21a767a1c3352b910
pvaiciunas <p.l.vaiciunas@gmail.com>
2014-08-26 17:16:49 -0400

    Completed main Spread strategies
    Currently need a lot of work. They function properly, but they're too
    specific to a 2 option option compbination. Will need to get a better
    framework so that you can expand to more complicated structures.

--- Option Strategies.r
+++ Option Strategies.r
@@ -4,58 +4,152 @@
 # take these lists of strategies and calcualte all other necessary info.
 # (i.e. payoffs, costs, exposures to greeks, etc)
 # Input is an option DF
-# Output is a 2 column data frame containing a list of options to buy
+# Output is a 2 column data frame with each record containing a list of options to buy
 # and a list of options to sell
 
+# These spread strategies are too specific at this point, they only really
+# allow for one ITM and one OTM option to be bought and sold. Will need to
+# generalize this a lot more so that more complex strategies can be modeled
 
+# Bull Call Spread
 BullCallSpread <- function(OptionDF) {
 #
 # Buy 1 ITM Call
 # Sell 1 OTM Call
 #
 # Input is a full OptionDF object
-# Output is a data.frame containing a list of tickers to buy and sell
+# Output is a data.frame columns denote the options to buy and sell
+# Each item in the dataframe is a list of option tickers
 
 # Clean up data and split the data frame into separate months
 	stockPrice <- OptionDF$stockPrice[1]
 	allCallOptions <- OptionDF[OptionDF$type == 'call',]
 	splitDF <- split(allCallOptions, allCallOptions$expiry)
-  
-	output <- data.frame()
+  	output <- data.frame()
   
   #for each ITM call, create all potential spreads inter-month
 	for (i in 1:length(splitDF)) {
-    
-		ITMOptions <- splitDF[[i]][splitDF[[i]]$strike <= stockPrice,]$optionCode
+  		ITMOptions <- splitDF[[i]][splitDF[[i]]$strike <= stockPrice,]$optionCode
 		OTMOptions <- splitDF[[i]][splitDF[[i]]$strike > stockPrice,]$optionCode
+  		possibleCombos <- expand.grid(ITMOptions, OTMOptions)
+		possibleCombosLists <- data.frame(I(split(possibleCombos[,1], rownames(possibleCombos))),
+								I(split(possibleCombos[,2], rownames(possibleCombos))))
     
-		possibleCombos <- expand.grid(ITMOptions, OTMOptions)
-    
-    possibleCombosLists <- data.frame(I(split(possibleCombos[,1], rownames(possibleCombos))),
-                                     I(split(possibleCombos[,2], rownames(possibleCombos))))
-    
-    output <- rbind(output, possibleCombosLists)
-    
-  }
-  
-  names(output) <- c("buy","sell")
-  rownames(output) <- 1:nrow(output)
-  output
-  
+		output <- rbind(output, possibleCombosLists)
+    }
+	
+	#Output
+	output[,3] <- "bull.call.spread"
+	names(output) <- c("buy","sell", "strategy")
+	rownames(output) <- 1:nrow(output)
+	output
 }
 
 
+# Bull Put Spread
+BullPutSpread <- function(OptionDF) {
+#
+# Buy 1 OTM Put
+# Sell 1 ITM Put
+#
+# Input is a full OptionDF object
+# Output is a data.frame columns denote the options to buy and sell
+# Each item in the dataframe is a list of option tickers
 
+# Clean up data and split the data frame into separate months
+	stockPrice <- OptionDF$stockPrice[1]
+	allCallOptions <- OptionDF[OptionDF$type == 'put',]
+	splitDF <- split(allCallOptions, allCallOptions$expiry)
+  	output <- data.frame()
+  
+  #for each ITM call, create all potential spreads inter-month
+	for (i in 1:length(splitDF)) {
+  		ITMOptions <- splitDF[[i]][splitDF[[i]]$strike <= stockPrice,]$optionCode
+		OTMOptions <- splitDF[[i]][splitDF[[i]]$strike > stockPrice,]$optionCode
+  		possibleCombos <- expand.grid(OTMOptions, ITMOptions)
+		possibleCombosLists <- data.frame(I(split(possibleCombos[,1], rownames(possibleCombos))),
+								I(split(possibleCombos[,2], rownames(possibleCombos))))
+    
+		output <- rbind(output, possibleCombosLists)
+    }
+	
+	#Output
+	output[,3] <- "bull.put.spread"
+	names(output) <- c("buy","sell", "strategy")
+	rownames(output) <- 1:nrow(output)
+	output
+}
 
-# Bear Call Spread
-
-# Bear Put Spread
 
 # Bull Put Spread
+BearCallSpread <- function(OptionDF) {
+#
+# Buy 1 OTM Call
+# Sell 1 ITM Call
+#
+# Input is a full OptionDF object
+# Output is a data.frame columns denote the options to buy and sell
+# Each item in the dataframe is a list of option tickers
 
-# Box Spread
+# Clean up data and split the data frame into separate months
+	stockPrice <- OptionDF$stockPrice[1]
+	allCallOptions <- OptionDF[OptionDF$type == 'call',]
+	splitDF <- split(allCallOptions, allCallOptions$expiry)
+  	output <- data.frame()
+  
+  #for each ITM call, create all potential spreads inter-month
+	for (i in 1:length(splitDF)) {
+  		ITMOptions <- splitDF[[i]][splitDF[[i]]$strike <= stockPrice,]$optionCode
+		OTMOptions <- splitDF[[i]][splitDF[[i]]$strike > stockPrice,]$optionCode
+  		possibleCombos <- expand.grid(OTMOptions, ITMOptions)
+		possibleCombosLists <- data.frame(I(split(possibleCombos[,1], rownames(possibleCombos))),
+								I(split(possibleCombos[,2], rownames(possibleCombos))))
+    
+		output <- rbind(output, possibleCombosLists)
+    }
+	
+	#Output
+	output[,3] <- "bear.call.spread"
+	names(output) <- c("buy","sell", "strategy")
+	rownames(output) <- 1:nrow(output)
+	output
+}
 
-# Long 
+
+# Bear Put Spread
+BearPutSpread <- function(OptionDF) {
+#
+# Buy 1 ITM Put
+# Sell 1 OTM Put
+#
+# Input is a full OptionDF object
+# Output is a data.frame columns denote the options to buy and sell
+# Each item in the dataframe is a list of option tickers
+
+# Clean up data and split the data frame into separate months
+	stockPrice <- OptionDF$stockPrice[1]
+	allCallOptions <- OptionDF[OptionDF$type == 'put',]
+	splitDF <- split(allCallOptions, allCallOptions$expiry)
+  	output <- data.frame()
+  
+  #for each ITM call, create all potential spreads inter-month
+	for (i in 1:length(splitDF)) {
+  		ITMOptions <- splitDF[[i]][splitDF[[i]]$strike <= stockPrice,]$optionCode
+		OTMOptions <- splitDF[[i]][splitDF[[i]]$strike > stockPrice,]$optionCode
+  		possibleCombos <- expand.grid(ITMOptions, OTMOptions)
+		possibleCombosLists <- data.frame(I(split(possibleCombos[,1], rownames(possibleCombos))),
+								I(split(possibleCombos[,2], rownames(possibleCombos))))
+    
+		output <- rbind(output, possibleCombosLists)
+    }
+	
+	#Output
+	output[,3] <- "bear.put.spread"
+	names(output) <- c("buy","sell", "strategy")
+	rownames(output) <- 1:nrow(output)
+	output
+}
+
 
 
 # Generalized Payoff Function
@@ -87,8 +181,11 @@ OptionPayoffPlot <- function(optionsCode, riskFreeRate) {
 	
 }
 
+#TODO
 ImpliedDividend <- function(){}
 
+#TODO
+# Part of the Arb strategies section
 PutCallParity <- function() {
 
 
